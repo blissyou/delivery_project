de061f47bb22b45b4628f0342ef8e1974de12c51
원형 <147372294+blissyou@users.noreply.github.com>
2025-03-19 00:54:19 +0900

    LoggerFilter 구현: 데이터가 어떤 메소드에서 어떤 uri로 어떤 헤더와 바디가 들어오는지

---
+++ api/src/main/java/org/delivery/api/config/jpa/filter/LoggerFilter.java
@@ -0,0 +1,66 @@
+package org.delivery.api.config.jpa.filter;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class LoggerFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest Request, ServletResponse Response, FilterChain Chain) throws IOException, ServletException {
+
+        var req = new ContentCachingRequestWrapper((HttpServletRequest) Request);
+        var res = new ContentCachingResponseWrapper((HttpServletResponse) Response);
+
+        Chain.doFilter(req, res);
+
+        // request 정보
+        var headersNames = req.getHeaderNames();
+        var headerValues = new StringBuilder();
+
+        headersNames.asIterator().forEachRemaining(headerKey -> {
+            var headerValue = req.getHeader(headerKey);
+
+            // authorization- token : ??? , user-agent: ???
+            headerValues.append("[")
+                    .append(headerKey)
+                    .append(":")
+                    .append(headerValue)
+                    .append("] ");
+
+
+        });
+
+        var requestBody = new String(req.getContentAsByteArray());
+
+        var uri = req.getRequestURI();
+        var method = req.getMethod();
+        log.info(">>>> uri: {}, method: {}, headers: {} , body: {}",uri,method ,headerValues, requestBody);
+
+        //response 정보
+        var responseHeaderValues = new StringBuilder();
+
+        res.getHeaderNames().forEach(headerKey -> {
+            var headerValue = res.getHeader(headerKey);
+
+            responseHeaderValues.append("[")
+                    .append(headerKey)
+                    .append(":")
+                    .append(headerValue)
+                    .append("] ");
+        });
+        var responseBody = new String(res.getContentAsByteArray());
+        log.info("<<<<uri{}, method: {}, header : {}, body : {}",uri,method, responseHeaderValues, responseBody);
+
+        res.copyBodyToResponse();
+    }
+}
